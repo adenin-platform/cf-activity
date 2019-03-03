6c5c0a7105748a680891f8f7e0edc3ab5bbf4312
Martin Amm <marint.amm@adenin.com>
2019-03-03 14:30:49 -0500

    added optional statuscodes[] for success and auth required

--- handleError.js
+++ handleError.js
@@ -6,10 +6,26 @@ const logger = require('@adenin/cf-logger');
  * logs the error and generates generic error response
  * @param {object} activity - the activity context
  * @param {error} error - the JavaScript error object
+ * @param {Number[]} authRequiresStatusCodes - optional array of statusCode recognized as success, default for token based auth [401]
  */
 
-module.exports = (activity, error) => {
-  logger.error(error);
+module.exports = (activity, error, authRequiresStatusCodes) => {
+
+  if(authRequiresStatusCodes == undefined) {
+
+    // if OAuth2 tokens are used status 401 should be mapped to 461 auth required
+    if(activity.Context.connector.token) {
+      authRequiresStatusCodes = [401];
+    } else {
+      authRequiresStatusCodes = [];
+    }
+  }
+
+  if(error.response && error.response.statusCode && authRequiresStatusCodes.indexOf(error.response.statusCode)>=0) {
+    error.response.statusCode = 461;
+  } else {  
+    logger.error(error);
+  }
 
   let m = error.message;
 
