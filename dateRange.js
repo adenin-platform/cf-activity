88ae6301ebbeb641de04c78183e49e0f67103605
olliejm <olliejm@protonmail.com>
2019-05-09 16:14:30 +0100

    add new daterange implementation

--- dateRange.js
+++ dateRange.js
@@ -10,19 +10,69 @@ module.exports = (activity, defaultRange) => {
   // optional: provide named default range
   if (defaultRange === undefined) defaultRange = 'today';
 
-  // just a PoC - todo: add validation, more ranges, support date only ...
-  let start = activity.Request.Query.startDate;
-  let end = activity.Request.Query.endDate;
+  const inputStartDate = activity.Request.Query.startDate;
+  const inputEndDate = activity.Request.Query.endDate;
 
-  if (!start || !end || start.length !== 24 || end.length !== 24) {
+  let start = null;
+  let end = null;
+
+  if (inputStartDate) {
+    start = extractDateFromString(activity, inputStartDate);
+    try {
+      start.setHours(0, 0, 0, 0);
+    } catch (error) {
+      $.handleError(activity, error);
+    }
+  }
+
+  if (inputEndDate) {
+    end = extractDateFromString(activity, inputEndDate);
+    try {
+      end.setHours(23, 59, 0, 0);
+    } catch (error) {
+      $.handleError(activity, error);
+    }
+  }
+
+  if (!start) {
     const today = new Date();
+    start = (new Date(today.setHours(0, 0, 0, 0)));
+  }
 
-    start = (new Date(today.setHours(0, 0, 0, 0))).toISOString();
-    end = (new Date(today.setHours(23, 59, 0, 0))).toISOString();
+  if (!end) {
+    const today = new Date();
+    end = (new Date(today.setHours(23, 59, 0, 0)));
   }
 
   return {
-    startDate: start,
-    endDate: end
+    startDate: start.toISOString(),
+    endDate: end.toISOString()
   };
 };
+
+function extractDateFromString(activity, input) {
+  let date = null;
+  let inputYear = null;
+  let inputMonth = null;
+  let inputDate = null;
+
+  if (input.length === 8) {
+    inputYear = input.substring(0, 4);
+    inputMonth = input.substring(4, 6);
+    inputDate = input.substring(6, 9);
+  } else if (input.includes('-')) {
+    const output = input.split('-');
+
+    inputYear = output[0];
+    inputMonth = output[1];
+    inputDate = output[2];
+  }
+
+  try {
+    date = new Date(inputYear, parseInt(inputMonth, 10) - 1, inputDate);
+  } catch (error) {
+    $.handleError(activity, error);
+  }
+
+  return date;
+}
